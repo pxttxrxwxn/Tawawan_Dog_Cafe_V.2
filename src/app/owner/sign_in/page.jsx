36222c51ed6e4b81696585f42447f4af9358904c
pxttxrxwxn <naysasatadur5555@gmail.com>
2025-07-22 13:27:32 +0700

    sign_in

---
+++ src/app/owner/sign_in/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link'
+
+function sign_in() {
+    return (
+        <div className="w-[1192px] h-[1000px] bg-white"></div>
+    )
+}
+
+export default sign_in
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#FDF6E3] flex items-center justify-between">
+    <div className="min-h-screen  flex items-center justify-between">
       <div className="flex flex-col items-left justify-center h-screen ml-[3%] mr-[3%]">
         <Image 
         src="/logo.png" alt="Logo" 
@@ -20,10 +21,12 @@ export default function Home() {
             </div>
           </div>
           <div className="flex flex-col justify-center items-center gap-y-6">
-            <div className="bg-[#C49A6C] w-[569px] h-[103px] rounded-2xl font-Inter text-[48px] text-[#FFFFFF] flex justify-center items-center 
-            shadow-md cursor-pointer hover:opacity-90 hover:text-black transform transition-transform duration-300 hover:scale-105">
-              Owner
-            </div>
+            <Link href="/owner/sign_in">
+              <div className="bg-[#C49A6C] w-[569px] h-[103px] rounded-2xl font-Inter text-[48px] text-[#FFFFFF] flex justify-center items-center 
+              shadow-md cursor-pointer hover:opacity-90 hover:text-black transform transition-transform duration-300 hover:scale-105">
+                Owner
+              </div>
+            </Link>
             <div className="text-[#D64545] font-Inter text-[20px] underline">
               สำหรับร้านคาเฟ่เท่านั้น
             </div>
