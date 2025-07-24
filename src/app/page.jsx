892b0947e6ddbfa32cf2ffe2f7fd5533d2dd2382
pxttxrxwxn <naysasatadur5555@gmail.com>
2025-07-24 08:46:22 +0700

    Index

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Index() {
   return (
     <div className="min-h-screen  flex items-center justify-between">
       <div className="flex flex-col items-left justify-center h-screen ml-[3%] mr-[3%]">
@@ -12,24 +12,24 @@ export default function Home() {
       <div className="bg-[#FFE8A3] h-screen w-[60%] flex flex-col items-right rounded-l-[100px]">
         <div className="flex flex-col justify-center items-center h-screen space-y-[5%]">
           <div className="flex flex-col justify-center items-center gap-y-6">
-            <div className="bg-[#8D6E63] w-[569px] h-[103px] rounded-2xl font-Inter text-[48px] text-[#FFFFFF] flex justify-center items-center 
+            <button className="bg-[#8D6E63] w-[569px] h-[103px] rounded-2xl font-Inter text-[48px] text-[#FFFFFF] flex justify-center items-center 
             shadow-md cursor-pointer hover:opacity-90 hover:text-black transform transition-transform duration-300 hover:scale-105">
               Customer
-            </div>
-            <div className="text-[#D64545] font-Inter text-[20px] underline">
+            </button>
+            <p className="text-[#D64545] font-Inter text-[20px] underline">
               สำหรับลูกค้าที่ต้องการสั่งอาหารในร้าน
-            </div>
+            </p>
           </div>
           <div className="flex flex-col justify-center items-center gap-y-6">
             <Link href="/owner/sign_in">
-              <div className="bg-[#C49A6C] w-[569px] h-[103px] rounded-2xl font-Inter text-[48px] text-[#FFFFFF] flex justify-center items-center 
+              <button className="bg-[#C49A6C] w-[569px] h-[103px] rounded-2xl font-Inter text-[48px] text-[#FFFFFF] flex justify-center items-center 
               shadow-md cursor-pointer hover:opacity-90 hover:text-black transform transition-transform duration-300 hover:scale-105">
                 Owner
-              </div>
+              </button>
             </Link>
-            <div className="text-[#D64545] font-Inter text-[20px] underline">
+            <p className="text-[#D64545] font-Inter text-[20px] underline">
               สำหรับร้านคาเฟ่เท่านั้น
-            </div>
+            </p>
           </div>
         </div>
       </div>
