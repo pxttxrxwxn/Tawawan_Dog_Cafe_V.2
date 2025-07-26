d24c4f7e467808ff0a50d1f019482fbc8fd1ee75
pxttxrxwxn <naysasatadur5555@gmail.com>
2025-07-26 16:08:01 +0700

    navbar

--- src/app/owner/order/page.jsx
+++ src/app/owner/order/page.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import Navbar from '@/app/components/Navbar'
+import Navbar from '../../components/Navbar'
 
 
-function order() {
+
+export default function order() {
   return (
     <div className="">
       <Navbar activePage="order" />
-      <div className="">ggggg</div>
     </div>
   )
 }
-
-export default order
