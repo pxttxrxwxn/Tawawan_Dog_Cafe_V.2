83b28fab124d9c191eaeb8de0442b2f912b5b05d
pxttxrxwxn <naysasatadur5555@gmail.com>
2025-08-29 13:40:16 +0700

    order_completed

---
+++ src/app/owner/order_completed/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+export default function oder_completed() {
+    return (
+        <div>
+            <Navbar activePage="order" />
+        </div>
+    )
+}
