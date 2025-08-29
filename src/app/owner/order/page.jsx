20affe6072378b53a61343743279422502787542
pxttxrxwxn <naysasatadur5555@gmail.com>
2025-08-29 08:30:47 +0700

    order

--- src/app/owner/order/page.jsx
+++ src/app/owner/order/page.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
-import TodayIcon from '@mui/icons-material/Today'
-import { Alarm, CheckBox, ContentPaste } from '@mui/icons-material'
-
 
 export default function order() {
   const orders = [
@@ -22,12 +19,24 @@ export default function order() {
 
       <div className="p-6 ">
         <div className="mb-4 mt-[200px] pl-[80px] pr-[80px] flex">
-          <div className="w-[454px] h-[99px] p-5 rounded-sm bg-[#FFE7A1] flex items-center">
-            <TodayIcon style={{ fontSize: '55px'}} />
+          <div className="w-[454px] h-[99px] p-5 rounded-sm bg-[#FFE7A1] flex items-center text-[#000000]">
+            <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            height="48px" 
+            viewBox="0 -960 960 960" 
+            width="48px" 
+            fill="#000000">
+              <path d="M352.82-310Q312-310 284-338.18q-28-28.19-28-69Q256-448 284.18-476q28.19-28 69-28Q394-504 422-475.82q28 28.19 28 69Q450-366 421.82-338q-28.19 28-69 28ZM180-80q-24 0-42-18t-18-42v-620q0-24 18-42t42-18h65v-60h65v60h340v-60h65v60h65q24 0 42 18t18 42v620q0 24-18 42t-42 18H180Zm0-60h600v-430H180v430Zm0-490h600v-130H180v130Zm0 0v-130 130Z"/></svg>
             <span className="font-bold text-[30px] m-5">คำสั่งซื้อจากลูกค้า</span>
           </div>
           <div className="ml-auto bg-[#0FA958] w-[454px] h-[99px] text-white px-5 py-2 rounded-lg flex items-center">
-            <CheckBox style={{ fontSize: '55px', color: 'black'}} />
+            <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            height="48px" 
+            viewBox="0 -960 960 960" 
+            width="48px" 
+            fill="#000000">
+              <path d="m419-321 289-290-43-43-246 247-119-119-43 43 162 162ZM180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h600q24 0 42 18t18 42v600q0 24-18 42t-42 18H180Z"/></svg>
             <span className="font-bold text-[30px] m-5">ออเดอร์เสร็จสิ้น</span>
           </div>
         </div>
@@ -36,24 +45,39 @@ export default function order() {
           {orders.map((order) => (
             <div key={order.id} className="bg-white w-[388px] h-[467px] p-4 rounded-lg shadow">
               <div className="flex justify-between mb-2">
-                <div className='flex flex-wrap'>
-                  <p className="text-[32px]">
-                    <ContentPaste style={{ fontSize: '32px'}} /> ออเดอร์ #{order.id}
-                    <p className="text-sm text-black">{order.date} | เวลา: {order.time}</p>
-                  </p>
-                  <p className="text-[30px] ml-[30px] mt-5">โต๊ะ: {order.table}</p>
+                <div className='flex justify-between'>
+                  <div className="flex flex-col">
+                    <p className="flex items-center gap-2 text-[32px] text-[#000000]">
+                      <svg 
+                      xmlns="http://www.w3.org/2000/svg" 
+                      height="32px" 
+                      viewBox="0 -960 960 960"
+                      width="32px" 
+                      fill="#000000">
+                        <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h167q11-35 43-57.5t70-22.5q40 0 71.5 22.5T594-840h166q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560h-80v120H280v-120h-80v560Zm280-560q17 0 28.5-11.5T520-800q0-17-11.5-28.5T480-840q-17 0-28.5 11.5T440-800q0 17 11.5 28.5T480-760Z"/></svg> 
+                        ออเดอร์ #{order.id}
+                    </p>
+                    <span className="text-sm text-black">{order.date} | เวลา: {order.time}</span>
+                  </div>
+                  <p className="text-[30px] ml-[30px] mt-5 text-[#000000]">โต๊ะ: {order.table}</p>
                 </div>
               </div>
               <hr className="my-2 ml-[-16px] text-gray-300 w-[388px]"/>
               <div>
-                <p className="font-semibold text-[40px]">รายการ</p>
+                <p className="font-semibold text-[40px] text-[#000000]">รายการ</p>
                 {order.items.map((item, index) => (
-                  <p className='ml-5' key={index}>{item}</p>
+                  <p className='ml-5 text-[#000000]' key={index}>{item}</p>
                 ))}
               </div>
               <hr className="my-2 ml-3 text-gray-300 w-[320px]"/>
               <div className="mt-[195px] flex items-center">
-                <Alarm />
+                <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                height="24px" 
+                viewBox="0 -960 960 960" 
+                width="24px" 
+                fill="#000000">
+                  <path d="M480-80q-75 0-140.5-28.5t-114-77q-48.5-48.5-77-114T120-440q0-75 28.5-140.5t77-114q48.5-48.5 114-77T480-800q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-440q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-80Zm112-192 56-56-128-128v-184h-80v216l152 152ZM224-866l56 56-170 170-56-56 170-170Zm512 0 170 170-56 56-170-170 56-56Z"/></svg>
                 <p className="font-bold text-[20px] text-[#F4A261] ml-2">
                   สถานะ: <span className="text-[#F4A261] font-bold">{order.status}</span>
                 </p>
