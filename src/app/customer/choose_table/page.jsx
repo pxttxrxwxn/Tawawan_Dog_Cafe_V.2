cf4787158cabad7d57586068f28d915e09010e54
pxttxrxwxn <naysasatadur5555@gmail.com>
2025-08-29 15:54:39 +0700

    00

--- src/app/customer/choose_table/page.jsx
+++ src/app/customer/choose_table/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function Index() {
+export default function choose_table() {
     return(
         <div className="min-h-screen bg-[#fdf6e3]">
             <div className="flex justify-start mt-[35px] ml-[20px]">
